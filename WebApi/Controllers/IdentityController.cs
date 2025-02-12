6169b749d026c35648046ba1fc0121d38c011846
Hora-keven <kevensantos993@gmail.com>
2025-02-12 14:31:24 -0300

    feat: identity class

---
+++ WebApi/Controllers/IdentityController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services.Token;
+
+namespace WebApi.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly TokenGenerator _tokenGenerator;
+        public IdentityController(TokenGenerator tokenGenerator){
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost("/login"), Authorize]
+        public ActionResult<LoginResponseModel> GenerateToken([FromBody] LoginRequestModel loginRequestModel)
+        {
+            var response = new LoginResponseModel() { Access_Token = _tokenGenerator.GenerateToken(loginRequestModel.Email!) };
+            return Ok(response);
+        }
+    }
+}
