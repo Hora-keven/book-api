79a2a9ac2a6ad388422c3f300a3d37b4ea07cd56
Hora-keven <kevensantos993@gmail.com>
2025-02-12 14:28:21 -0300

    feat: jwt tokem authentication

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,11 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.Data;
 using WebApi.Services.AuthorS;
 using WebApi.Services.BookS;
+using WebApi.Services.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +15,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IAuthorInterface, AuthorService>();
 builder.Services.AddScoped<IBookInterface, BookService>();
+builder.Services.AddSingleton<TokenGenerator>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme
+).AddJwtBearer(options =>
+   {
+       options.RequireHttpsMetadata = false;
+       options.SaveToken = true;
+          options.TokenValidationParameters = new TokenValidationParameters
+          {
+              ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+              ValidAudience = builder.Configuration["JwtSettings:Audience"],
+              IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!)),
+              ValidateIssuer = true,
+              ValidateAudience = true,
+              ValidateLifetime = true,
+              ValidateIssuerSigningKey = true,
+          };
+   }
+);
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
