c6aebfbba4008d78a6c6634e79cedd8bc9baac31
Hora-keven <kevensantos993@gmail.com>
2025-02-12 14:30:45 -0300

    feat: authorize

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Dto.Book;
 using WebApi.Models;
@@ -10,6 +11,7 @@ using WebApi.Services.BookS;
 namespace WebApi.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("api/[controller]")]
     public class BookController : ControllerBase
     {
@@ -19,28 +21,33 @@ namespace WebApi.Controllers
             _bookInterface = bookInterface;
         }
         [HttpPost("/book")]
-        public async Task<ActionResult<ResponseModel<List<Book>>>> CreateBook(CreateBookDto createBookDto){
+        public async Task<ActionResult<ResponseModel<List<Book>>>> CreateBook(CreateBookDto createBookDto)
+        {
             var books = await _bookInterface.CreateBook(createBookDto);
             var locationUri = Url.Action("GetBook", new { id = createBookDto.Title });
             return Created(locationUri, books);
         }
         [HttpGet("/books")]
-        public async Task<ActionResult<ResponseModel<List<Book>>>> GetAllBooks(){
+        public async Task<ActionResult<ResponseModel<List<Book>>>> GetAllBooks()
+        {
             var books = await _bookInterface.GetAllBooks();
             return Ok(books);
         }
         [HttpDelete("/{bookId}")]
-        public async Task<ActionResult<ResponseModel<List<Book>>>> DeleteBook(int bookId){
+        public async Task<ActionResult<ResponseModel<List<Book>>>> DeleteBook(int bookId)
+        {
             await _bookInterface.DeleteBook(bookId);
             return NoContent();
         }
         [HttpPut("/editBook")]
-        public async Task<ActionResult<ResponseModel<List<Book>>>> EditBook(EditBookDto editBookDto){
+        public async Task<ActionResult<ResponseModel<List<Book>>>> EditBook(EditBookDto editBookDto)
+        {
             var books = await _bookInterface.EditBook(editBookDto);
             return Ok(books);
         }
         [HttpGet("/search/{bookId}")]
-        public async Task<ActionResult<ResponseModel<Book>>> GetBookId(int bookId){
+        public async Task<ActionResult<ResponseModel<Book>>> GetBookId(int bookId)
+        {
             var book = await _bookInterface.GetBookById(bookId);
             return Ok(book);
         }
